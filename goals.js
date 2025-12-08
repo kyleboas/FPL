ccf4c01e494a0ea872f3c98da2e3c796bfc1fe66
Kyle Boas <68482183+kyleboas@users.noreply.github.com>
2025-12-08 12:56:51 -0500

    .

--- goals.js
+++ goals.js
@@ -653,7 +653,7 @@ function renderTable() {
                 td.style.backgroundColor = getGoalsColor(cell.value, statType, globalMaxValue);
 
                 // Adjust text color for readability
-                if (cell.value < 0.8) {
+                if (cell.value <= 0.8) {
                     // Black for values below 0.8
                     divOpp.style.color = '#000';
                     divValue.style.color = '#000';
